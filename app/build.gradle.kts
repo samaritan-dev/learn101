dca298540a5f87906cdaf9a35402dbaac41c695e
sk.samar <sk.samar@quantumgate.co.uk>
2025-01-27 14:11:25 +0000

    ubantu with actions 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "quantumgate.learning.learn101"
-        minSdk = 30
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
