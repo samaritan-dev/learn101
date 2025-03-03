6c261ef8e2e605aed0482c18b4076f7da60a840b
sk.samar <sk.samar@quantumgate.co.uk>
2025-03-03 14:58:37 +0000

    test 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "quantumgate.learning.learn101"
-        minSdk = 29
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
